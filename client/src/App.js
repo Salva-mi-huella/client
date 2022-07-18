b4aa2d2772b7e20a2fc617a2a7f6913dad2d3ded
Joaquin Tomas Gioffre <j.gioffre@hotmail.com>
2022-07-18 15:51:34 -0300

    inital commit

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+
+
+function App() {
+  return (
+    <Switch>
+    <div className="App">
+
+      <h1>SALVA MI HUELLA  juliano</h1>
+      <h1>SALVA MI HUELLA 4.0</h1>
+
+    </div>
+    </Switch>
+  );
+}
+
+export default App;
---
+++ client/src/reducers/index.js
@@ -0,0 +1,15 @@
+// IMPORT ACTIONS HERE
+
+
+const initialState = {
+   
+}
+
+export default function rootReducer(state = initialState, action) {
+
+    switch(action.type) {
+
+        default: return {...state}
+
+    }
+}
