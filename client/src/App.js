0dbb71262d08b254ba028b3163c0402a3987fb75
Joaquin Tomas Gioffre <j.gioffre@hotmail.com>
2022-07-19 19:57:26 -0300

    refactor/component_foundation && action-reducer

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ function App() {
    <Route path='/'> <NavBar/></Route>
    {/* <Route exact path='/home'> <Home/></Route> */}
    {/* <Route exact path='/perfil'> <Profile/></Route> */}
-   <Route path='/fundacion'  component={Foundation}/> {/*Cambiar ruta a fundacion/:foundationId una vez que funcione la action*/}
+   <Route path='/fundacion/:foundationId'  component={Foundation}/> {/*Cambiar ruta a fundacion/:foundationId una vez que funcione la action*/}
    {/* <Route path='/salva-una-huella'  component={Adopt}/> */}
    {/* <Route exact path='/donaciones' component={Donate} /> */}
    {/* <Route exact path='/nosotros' component={About} /> */}
