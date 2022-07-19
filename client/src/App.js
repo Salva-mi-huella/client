ec4709b39036dcfc8077af9771c63240b25b50de
jargumedo <juliargumedoblanco@gmail.com>
2022-07-19 14:20:48 -0300

    component petDetail

--- client/src/App.js
+++ client/src/App.js
@@ -3,21 +3,22 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import {Home} from './components/Home/Home';
 import NavBar from './components/NavBar/NavBar';
+import PetDetail from './components/PetDetail/PetDetail';
 
 
 function App() {
   return (
-    <Switch>
+    <React.Fragment>
    <Route path='/'> <NavBar/></Route>
+   <Route exact path='/huella'> <PetDetail/></Route>
    {/* <Route exact path='/home'> <Home/></Route> */}
-   {/* <Route exact path='/huella/:nombreHuella'> <PetDetail/></Route> */}
    {/* <Route exact path='/perfil'> <Profile/></Route> */}
    {/* <Route path='/fundacion/:fundacionId'  component={Foundation}/> */}
    {/* <Route path='/salva-una-huella'  component={Adopt}/> */}
    {/* <Route exact path='/donaciones' component={Donate} /> */}
    {/* <Route exact path='/nosotros' component={About} /> */}
    {/* <Route component={NotFound}  /> */}
-    </Switch>
+    </React.Fragment>
   );
 }
 
