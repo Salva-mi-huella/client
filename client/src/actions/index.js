9f201fcab5d2bc9c8b85cae67788933f94aba8e4
Santiago Seisdedos <santiagoseisdedos@hotmail.com>
2022-07-20 20:54:45 -0300

    No cambie nada

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -5,8 +5,8 @@ export const GET_FOUNDATION_DETAIL = 'GET_FOUNDATION_DETAIL';
 export const GET_PET_DETAIL = 'GET_PET_DETAIL';
 export const GET_FOUNDATIONS = 'GET_FOUNDATIONS';
 
-export function getFoundationDetail(id){
-    return function(dispatch) {
+export function getFoundationDetail(id) {
+    return function (dispatch) {
         // try{
         //     return axios(`/foundations/${id}`)
         //     .then(detail =>
@@ -14,12 +14,12 @@ export function getFoundationDetail(id){
         //     } catch (e) {
         //         dispatch({ type: GET_FOUNDATION_DETAIL, payload: e.data })
         //     }
-            return dispatch({ type: GET_FOUNDATION_DETAIL, payload: data.foundation.filter(foundation => foundation.id === id) })
-        } 
+        return dispatch({ type: GET_FOUNDATION_DETAIL, payload: data.foundation.filter(foundation => foundation.id === id) })
+    }
 }
 
-export function getFoundations(){
-    return function(dispatch) {
+export function getFoundations() {
+    return function (dispatch) {
         // try{
         //     return axios(`/foundations`)
         //     .then(detail =>
@@ -27,18 +27,17 @@ export function getFoundations(){
         //     } catch (e) {
         //         dispatch({ type: GET_FOUNDATIONS, payload: e.data })
         //     }
-            return dispatch({ type: GET_FOUNDATIONS, payload: data.foundation})
-        } 
-    }
-
-export function getPetDetail(name){
-    console.log("hola")
-    return function (dispatch){
-        return dispatch({
-            type: GET_PET_DETAIL,
-            payload: data.pet.find(animal => animal.name.toLowerCase()=== name.toLowerCase())
-        })
+        return dispatch({ type: GET_FOUNDATIONS, payload: data.foundation })
     }
 }
+
+export function getPetDetail(name) {
+    console.log("hola")
+    return function (dispatch) {
+        return dispatch({
+            type: GET_PET_DETAIL,
+            payload: data.pet.find(animal => animal.name.toLowerCase() === name.toLowerCase())
+        })
+    }
 }
 
