2f604b8c02f9aa65b8b7dbf5afbaecfb5b5b46aa
Joaquin Tomas Gioffre <j.gioffre@hotmail.com>
2022-07-22 00:09:25 -0300

    refactor/component_donate_info

--- client/src/components/Donate/Donate.js
+++ client/src/components/Donate/Donate.js
@@ -5,7 +5,7 @@ import { useAuth0 } from '@auth0/auth0-react';
 import style from './Donate.module.css'
 import { getFoundations } from '../../actions/index.js';
 import Stepper from './Stepper';
-import banner from '../../assets/adoptBanner.png'
+import banner from '../../assets/paw_hand.png'
 import product_a from '../../assets/product_a.jpg'
 import product_b from '../../assets/product_b.jpg'
 import product_c from '../../assets/product_c.png'
@@ -38,7 +38,7 @@ export default function Donate(){
                     <h1>Tu ayuda puede salvar una huella</h1>
                     <br></br>
                     <p>Tu aporte económico es muy importante para que las fundaciones puedan pagar tratamientos, estudios médicos y alimentos para ayudar a nuestros peludos amigos.
-                    <br></br><br></br> Agradecemos tu apoyo, y sabemos que nuestras huellas también!</p>
+                    <br></br><br></br> Agradecemos tu apoyo y sabemos que nuestras huellas también!</p>
                 </div>
                 {/* Cambiar esta imagen */}
                 <img src={banner} alt="Banner of animals"/>
@@ -46,15 +46,9 @@ export default function Donate(){
 
             <div className={style.containerA}>
                 <div className={style.subcontainerA}>
-                    <h3>¿Cómo donar?</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                        Illum modi repudiandae,eligendi ipsum, quod  Lorem ipsum 
-                        dolor sit amet consectetur adipisicing elit. 
+                    <h3>Disfrutá de Huellitas</h3>
+                    <p>Nuestro programa gratuito en el que podés ganar puntos por cada donación a nuestras fundaciones y canjear por productos en nuestra tienda! 
                     </p>
-                </div>
-                <div className={style.subcontainerA}>
-                    <h4>¿Todavía no te registraste?</h4>
-                    <p>Hacelo gratis y sumá huellitas por cada donación para canjear por productos en nuestra tienda!</p>
                     <div>
                         <img src={product_a} alt='product_a'></img>
                         <span></span>
@@ -62,7 +56,12 @@ export default function Donate(){
                         <span></span>
                         <img src={product_c} alt='product_b'></img>
                         <span></span>
+                    <button>Ver más</button>
                     </div>
+                </div>
+                <div className={style.subcontainerB}>
+                    <h4>¿Todavía no te registraste?</h4>
+                    <p>Hacelo gratis y empezá a sumar huellitas!</p>
                     <button onClick={()=>loginWithRedirect()}>Registrarse</button>
                 </div>
             </div>
