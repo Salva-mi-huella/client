4b8deed9530856fe20961f924f5d3e0356988260
Santiago Seisdedos <santiagoseisdedos@hotmail.com>
2022-07-31 05:45:16 -0300

    refactor -> before Card Adjust

--- client/src/components/ProfileFoundation/Updates/Updates.jsx
+++ client/src/components/ProfileFoundation/Updates/Updates.jsx
@@ -1,26 +1,113 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
 import { UpdatesData } from '../Data/Data';
-import './Updates.css';
+import styles from './Updates.module.css';
 
 const Updates = () => {
+
+    const user = JSON.parse(localStorage.getItem('user'));
+    // const { user } = useAuth0();
+    // const dispatch = useDispatch();
+    let foundation = useSelector(state => state.foundations);
+
+    if (user) {
+        // console.log(user, 'user info');
+        foundation = foundation.find(f => f.email === user.email);
+        console.log(foundation, 'foundation info');
+    }
+
+    // React.useEffect(() => {
+    //     dispatch(getFoundations());
+    // }, [dispatch])
+
     return (
-        <div className="Updates">
-            {UpdatesData.map((update) => {
+        <div className={styles.updatedNewsContainer}>
+            {foundation?.news.map((update) => {
                 return (
-                    <div className="update">
-                        <img src={update.img} alt='User not available' />
-                        <div className="news">
+                    <div className={styles.singleUpdateContainer}>
+                        <img src={update.images[0]} alt='User not available' />
+                        <div className={styles.singleNew}>
                             <div style={{ marginBottom: '0.5rem' }}>
-                                <span>{update.name}</span>
-                                <span> {update.news}</span>
+                                <p>{update.title}</p>
+                                <span> {update.little_description}</span>
                             </div>
-                            <span>{update.time}</span>
+                            <span>{update.post_date}</span>
                         </div>
 
 
                     </div>
                 )
             })}
+
+            {foundation?.news.map((update) => {
+                return (
+                    <div className={styles.singleUpdateContainer}>
+                        <img src={update.images[0]} alt='User not available' />
+                        <div className={styles.singleNew}>
+                            <div style={{ marginBottom: '0.5rem' }}>
+                                <p>{update.title}</p>
+                                <span> {update.little_description}</span>
+                            </div>
+                            <span>{update.post_date}</span>
+                        </div>
+
+
+                    </div>
+                )
+            })}
+
+            {foundation?.news.map((update) => {
+                return (
+                    <div className={styles.singleUpdateContainer}>
+                        <img src={update.images[0]} alt='User not available' />
+                        <div className={styles.singleNew}>
+                            <div style={{ marginBottom: '0.5rem' }}>
+                                <p>{update.title}</p>
+                                <span> {update.little_description}</span>
+                            </div>
+                            <span>{update.post_date}</span>
+                        </div>
+
+
+                    </div>
+                )
+            })}
+
+            {foundation?.news.map((update) => {
+                return (
+                    <div className={styles.singleUpdateContainer}>
+                        <img src={update.images[0]} alt='User not available' />
+                        <div className={styles.singleNew}>
+                            <div style={{ marginBottom: '0.5rem' }}>
+                                <p>{update.title}</p>
+                                <span> {update.little_description}</span>
+                            </div>
+                            <span>{update.post_date}</span>
+                        </div>
+
+
+                    </div>
+                )
+            })}
+
+            {foundation?.news.map((update) => {
+                return (
+                    <div className={styles.singleUpdateContainer}>
+                        <img src={update.images[0]} alt='User not available' />
+                        <div className={styles.singleNew}>
+                            <div style={{ marginBottom: '0.5rem' }}>
+                                <p>{update.title}</p>
+                                <span> {update.little_description}</span>
+                            </div>
+                            <span>{update.post_date}</span>
+                        </div>
+
+
+                    </div>
+                )
+            })}
+
         </div>
     )
 }
