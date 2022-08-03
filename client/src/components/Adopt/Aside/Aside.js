6713e5eaf9a1be295589a3198f1dc4cecfab0204
NicolasPlanas1998 <nicolasplanas1998@gmail.com>
2022-08-03 15:14:19 -0300

    delete filterAside

--- client/src/components/Adopt/Aside/Aside.js
+++ client/src/components/Adopt/Aside/Aside.js
@@ -7,9 +7,8 @@ export default function Aside(){
 
     const [statusLike, setStatusLike] = useState(false)
     const arrAllPets = useSelector(state=> state.allPets )
+
     const dispatch = useDispatch()
-
-
     function handleLikes (){
         const favs = JSON.parse(localStorage.getItem('fav'));
         if(statusLike){
@@ -19,31 +18,16 @@ export default function Aside(){
         let collectionFavs = []
         for( let pet of arrAllPets ){
             for(let fav of favs){
-                if(pet.name === fav) collectionFavs.push(pet)
+                if(pet.id === fav) collectionFavs.push(pet)
             }}
         dispatch(petsFiltered(collectionFavs)) 
         setStatusLike(true)
     }}
 
-    //Limpiar filtros
-    function handleClean(){
-        dispatch(petsFiltered(arrAllPets)) 
-    }
     return(
         <div className={style.containerAside}>
             <div onClick={handleLikes} className={style.itemAside}>
                 <i className="fa-solid fa-heart"></i>
-            </div>
-            <div className={style.itemAsideFilter} >
-                <i className="fa-solid fa-filter"></i>
-                <div className={style.containerItems}>
-                    <p className={style.itemFiltTitle}>Filtros</p>
-                    <div >
-                        <span className={style.itemFilt}><i className="fa-solid fa-circle-xmark"></i>Gatos</span>
-                        <span className={style.itemFilt}><i className="fa-solid fa-circle-xmark"></i>Macho</span>
-                    </div>
-                    <button onClick={handleClean} className={style.cleanFilters}>Limpiar</button>
-                </div>
             </div>
         </div>
     )
