797aa2b1b6154befba4f65efb236657a3553b7b5
Santiago Seisdedos <santiagoseisdedos@hotmail.com>
2022-08-05 23:51:09 -0300

    refactor/: postPet swal + dispatch; cleaned bugs

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -18,10 +18,10 @@ export const POST_DONATION = 'POST_DONATION'
 export const POST_REQUEST_FOUNDATION = 'POST_REQUEST_FOUNDATION'
 export const GET_REQUESTS_FOUNDATIONS = 'GET_REQUESTS_FOUNDATIONS'
 export const GET_REQUESTS_ADOPT = 'GET_REQUESTS_ADOPT'
-export const ADD_TO_CART= 'ADD_TO_CART'
-export const DELETE_ONE_FROM_CART= 'DELETE_ONE_FROM_CART'
-export const DELETE_ALL_FROM_CART= 'DELETE_ALL_FROM_CART'
-export const CLEAR_CART= 'CLEAR_CART'
+export const ADD_TO_CART = 'ADD_TO_CART'
+export const DELETE_ONE_FROM_CART = 'DELETE_ONE_FROM_CART'
+export const DELETE_ALL_FROM_CART = 'DELETE_ALL_FROM_CART'
+export const CLEAR_CART = 'CLEAR_CART'
 export const GET_ALL_NEWS = 'GET_ALL_NEWS'
 export const UPDATE_PRODUCT = 'UPDATE_PRODUCT'
 export const GET_DONATIONS = 'GET_DONATIONS'
@@ -29,115 +29,115 @@ export const POST_PRODUCT = 'POST_PRODUCT'
 export const UPDATE_REQUEST_FOUNDATION = 'UPDATE_REQUEST_FOUNDATION'
 
 
-export function getFoundationDetail(id){
-    return function(dispatch) {
-        try{
+export function getFoundationDetail(id) {
+    return function (dispatch) {
+        try {
             return axios(`/foundations/${id}`)
-            .then(detail =>
-                dispatch({ type: GET_FOUNDATION_DETAIL, payload: detail.data }))
-            } catch (e) {
-                dispatch({ type: GET_FOUNDATION_DETAIL, payload: e.data })
-            }
-            // return dispatch({ type: GET_FOUNDATION_DETAIL, payload: data.foundation.filter(foundation => foundation.id === id) })
-        } 
+                .then(detail =>
+                    dispatch({ type: GET_FOUNDATION_DETAIL, payload: detail.data }))
+        } catch (e) {
+            dispatch({ type: GET_FOUNDATION_DETAIL, payload: e.data })
+        }
+        // return dispatch({ type: GET_FOUNDATION_DETAIL, payload: data.foundation.filter(foundation => foundation.id === id) })
+    }
 }
 
-export function getProductDetail(id){
-    return function(dispatch) {
-        try{
+export function getProductDetail(id) {
+    return function (dispatch) {
+        try {
             return axios(`/products/${id}`)
-            .then(detail =>
-                dispatch({ type: GET_PRODUCT_DETAIL, payload: detail.data }))
-            } catch (e) {
-                dispatch({ type: GET_PRODUCT_DETAIL, payload: e.data })
-            }
-        } 
+                .then(detail =>
+                    dispatch({ type: GET_PRODUCT_DETAIL, payload: detail.data }))
+        } catch (e) {
+            dispatch({ type: GET_PRODUCT_DETAIL, payload: e.data })
+        }
+    }
 }
 
-export function getFoundations(){
-    return function(dispatch) {
-        try{
+export function getFoundations() {
+    return function (dispatch) {
+        try {
             return axios(`/foundations`)
-            .then(detail =>
-                dispatch({ type: GET_FOUNDATIONS, payload: detail.data }))
-            } catch (e) {
-                dispatch({ type: GET_FOUNDATIONS, payload: e.data })
-            }
-        } 
+                .then(detail =>
+                    dispatch({ type: GET_FOUNDATIONS, payload: detail.data }))
+        } catch (e) {
+            dispatch({ type: GET_FOUNDATIONS, payload: e.data })
+        }
+    }
 
 }
 
-export function getAllPets(){
-    return async function (dispatch){
+export function getAllPets() {
+    return async function (dispatch) {
         try {
             const info = await axios("/pets")
             return dispatch({
-                type:GET_ALL_PETS,
+                type: GET_ALL_PETS,
                 payload: info.data
             })
         } catch (error) {
             // return dispatch({     
-                // Agregar componente de pagina en construccion
-                // Por si algun dia se cae la DB
+            // Agregar componente de pagina en construccion
+            // Por si algun dia se cae la DB
             // })
         }
     }
 }
 
-export function getAllNews(){
-    return async function (dispatch){
+export function getAllNews() {
+    return async function (dispatch) {
         try {
             const news = await axios("/news")
             return dispatch({
-                type:GET_ALL_NEWS,
+                type: GET_ALL_NEWS,
                 payload: news.data
             })
         } catch (error) {
             // return dispatch({     
-                // Agregar componente de pagina en construccion
-                // Por si algun dia se cae la DB
+            // Agregar componente de pagina en construccion
+            // Por si algun dia se cae la DB
             // })
         }
     }
 }
 
 
-export  function getPetDetail(id){
-    return async function (dispatch){
-        try{
+export function getPetDetail(id) {
+    return async function (dispatch) {
+        try {
             const info = await axios(`/pets/${id}`)
             return dispatch({
                 type: GET_PET_DETAIL,
                 payload: info.data
             })
         }
-        catch(error){
+        catch (error) {
             // return dispatch({     
-                // Agregar componente de pagina en construccion
-                // Por si algun dia se cae la DB
+            // Agregar componente de pagina en construccion
+            // Por si algun dia se cae la DB
             // })
         }
     }
 }
 
-export function filtersConfig(config){
+export function filtersConfig(config) {
     return {
         type: FILTERS_CONFIG,
-        filter : Object.keys(config),
+        filter: Object.keys(config),
         payload: config
-    } 
+    }
 }
-export function petsFiltered(pets,page){
+export function petsFiltered(pets, page) {
     return {
         type: PETS_FILTERED,
         filtered: pets,
         perPage: page
-    } 
+    }
 }
 
 
-export function getCurrency(){
-    return async function (dispatch){
+export function getCurrency() {
+    return async function (dispatch) {
         try {
             const info = await axios.get("https://www.dolarsi.com/api/api.php?type=valoresprincipales")
             return dispatch({
@@ -146,8 +146,8 @@ export function getCurrency(){
             })
         } catch (error) {
             // return dispatch({     
-                // Agregar componente de pagina en construccion
-                // Por si algun dia se cae la DB
+            // Agregar componente de pagina en construccion
+            // Por si algun dia se cae la DB
             // })
         }
     }
@@ -169,7 +169,7 @@ export function postUser(user) {
     }
 }
 
-export function updateFoundation(data,id) {
+export function updateFoundation(data, id) {
     return async function (dispatch) {
         try {
             const updatedFoundation = await axios.put(`/foundations/${id} `, data);
@@ -187,10 +187,10 @@ export function updateFoundation(data,id) {
 
 export function updateUser(data, email) {
     return async function (dispatch) {
-        console.log(data, email,"------")
+        console.log(data, email, "------")
         try {
             const updatedUser = await axios.put(`/users/${email}`, data);
-            
+
 
             return dispatch({
                 type: UPDATE_USER,
@@ -219,29 +219,29 @@ export function getUserByEmail(email) {
     }
 }
 
-export function getAllProducts(){
-    return async function (dispatch){
+export function getAllProducts() {
+    return async function (dispatch) {
         try {
             const info = await axios("/products")
             return dispatch({
-                type:GET_ALL_PRODUCTS,
+                type: GET_ALL_PRODUCTS,
                 payload: info.data
             })
         } catch (error) {
             // return dispatch({     
-                // Agregar componente de pagina en construccion
-                // Por si algun dia se cae la DB
+            // Agregar componente de pagina en construccion
+            // Por si algun dia se cae la DB
             // })
         }
     }
 }
 
-export function getRequestsFoundations(){
-    return async function (dispatch){
+export function getRequestsFoundations() {
+    return async function (dispatch) {
         try {
             const info = await axios("/request_foundations")
             return dispatch({
-                type:GET_REQUESTS_FOUNDATIONS,
+                type: GET_REQUESTS_FOUNDATIONS,
                 payload: info.data
             })
         } catch (error) {
@@ -250,25 +250,25 @@ export function getRequestsFoundations(){
     }
 }
 
-export function getRequestsAdopt(){
-    return async function (dispatch){
+export function getRequestsAdopt() {
+    return async function (dispatch) {
         try {
             const info = await axios("/request_adopts")
             return dispatch({
-                type:GET_REQUESTS_ADOPT,
+                type: GET_REQUESTS_ADOPT,
                 payload: info.data
             })
         } catch (error) {
             // return dispatch({     
-                // Agregar componente de pagina en construccion
-                // Por si algun dia se cae la DB
+            // Agregar componente de pagina en construccion
+            // Por si algun dia se cae la DB
             // })
         }
     }
 }
 
-export function postDonation(order){
-    return async function (dispatch){
+export function postDonation(order) {
+    return async function (dispatch) {
         try {
             const info = await axios.post("/donations", order)
             return dispatch({
@@ -277,26 +277,31 @@ export function postDonation(order){
             })
         } catch (error) {
             // return dispatch({     
-                // Agregar componente de pagina en construccion
-                // Por si algun dia se cae la DB
+            // Agregar componente de pagina en construccion
+            // Por si algun dia se cae la DB
             // })
         }
     }
 }
 // POST DB
-export function postNews (data){
-    return  function (){
-            axios.post("/news", data)
+export function postNews(data) {
+    return function () {
+        axios.post("/news", data)
     }
 }
-export function postPets (data){
-    return  function (){
+export function postPets(data) {
+    console.log(data);
+    return function () {
+        try {
             axios.post("/pets", data)
+        } catch (error) {
+            console.log(error);
+        }
     }
 }
-export function postRequestAdopt (data){
-    return function(){
-        try{
+export function postRequestAdopt(data) {
+    return function () {
+        try {
             axios.post("/request_adopts", data)
         }
         catch (e) {
@@ -305,9 +310,9 @@ export function postRequestAdopt (data){
     }
 }
 
-export function postMessage (data){
-    return function(){
-        try{
+export function postMessage(data) {
+    return function () {
+        try {
             axios.post("/messages", data)
         }
         catch (e) {
@@ -316,12 +321,12 @@ export function postMessage (data){
     }
 }
 
-export function getUsers(){
-    return async function (dispatch){
+export function getUsers() {
+    return async function (dispatch) {
         try {
             const allUsers = await axios("/users")
             return dispatch({
-                type:GET_USERS,
+                type: GET_USERS,
                 payload: allUsers.data
             })
         } catch (error) {
@@ -330,8 +335,8 @@ export function getUsers(){
     }
 }
 
-export function postRequestFoundation(data){
-    return async function (dispatch){
+export function postRequestFoundation(data) {
+    return async function (dispatch) {
         try {
             const info = await axios.post("/request_foundations", data)
             return dispatch({
@@ -340,8 +345,8 @@ export function postRequestFoundation(data){
             })
         } catch (error) {
             // return dispatch({     
-                // Agregar componente de pagina en construccion
-                // Por si algun dia se cae la DB
+            // Agregar componente de pagina en construccion
+            // Por si algun dia se cae la DB
             // })
         }
     }
@@ -349,42 +354,43 @@ export function postRequestFoundation(data){
 
 //SHOPPING CART
 
-export function addToCart(id){
-    return function(dispatch){
+export function addToCart(id) {
+    return function (dispatch) {
         dispatch({
-        type:ADD_TO_CART,
-        payload:id
-    })
-    }
-}
-
-export function delFromCart(id, all=false){
-    return function(dispatch){
-    if(all){
-    return dispatch({
-        type:DELETE_ALL_FROM_CART,
-        payload:id
-    })}else{
-        return dispatch({
-            type:DELETE_ONE_FROM_CART,
-            payload:id
+            type: ADD_TO_CART,
+            payload: id
         })
     }
+}
+
+export function delFromCart(id, all = false) {
+    return function (dispatch) {
+        if (all) {
+            return dispatch({
+                type: DELETE_ALL_FROM_CART,
+                payload: id
+            })
+        } else {
+            return dispatch({
+                type: DELETE_ONE_FROM_CART,
+                payload: id
+            })
+        }
     }
 }
 
-export function clearCart(){
-    return function(dispatch){
+export function clearCart() {
+    return function (dispatch) {
         return dispatch({
-        type:CLEAR_CART
-    })
+            type: CLEAR_CART
+        })
     }
 }
 
 // UPDATE PRODUCTS
 
 export function updateProduct(data, id) {
-    
+
     return async function (dispatch) {
         try {
             const updatedProduct = await axios.put(`/products/${id}`, data);
@@ -401,12 +407,12 @@ export function updateProduct(data, id) {
 }
 
 // GET DONATIONS
-export function getDonations(){
-    return async function (dispatch){
+export function getDonations() {
+    return async function (dispatch) {
         try {
             const allDonations = await axios("/donations")
             return dispatch({
-                type:GET_DONATIONS,
+                type: GET_DONATIONS,
                 payload: allDonations.data
             })
         } catch (error) {
@@ -415,9 +421,9 @@ export function getDonations(){
     }
 }
 //POST PRODUCTS
-export function postProduct(data){
+export function postProduct(data) {
     console.log(data)
-    return async function (dispatch){
+    return async function (dispatch) {
         try {
             const info = await axios.post("/products", data)
             return dispatch({
@@ -428,7 +434,7 @@ export function postProduct(data){
             console.log(error)
         }
     }
-} 
+}
 //UPDATE REQUEST_FOUNDATIONS
 export function updateRequestFoundation(data, id) {
     return async function (dispatch) {
